54749e4ecc35bf335d738333fa18a049a75337a7
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-06-03 15:26:07 +0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from src.database import db
-from blueprints.cities import bp as bp_cities
-from blueprints.colors import bp as bp_color
-from blueprints.image import bp as bp_image
+from src.blueprints.cities import bp as bp_cities
+from src.blueprints.colors import bp as bp_color
+from src.blueprints.image import bp as bp_image
+from src.blueprints.auth import bp as bp_auth
+from src.blueprints.users import bp as users_bp
+from src.blueprints.ads import bp as ads_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    app.register_blueprint(bp_image, url_prefix='/users')
-    app.register_blueprint(bp_image, url_prefix='/auth')
+    app.register_blueprint(users_bp, url_prefix='/users')
+    app.register_blueprint(bp_auth, url_prefix='/auth')
     app.register_blueprint(bp_color, url_prefix='/color')
     app.register_blueprint(bp_cities, url_prefix='/cities')
     app.register_blueprint(bp_image, url_prefix='/image')
-    app.register_blueprint(bp_image, url_prefix='/ads')
+    app.register_blueprint(ads_bp, url_prefix='/ads')
     db.init_app(app)
     return app
